4f3c34cd5c1268c4e148ab9f8ff9d852d6335c8f
Tony Locke <tony.locke@wessexwater.co.uk>
2014-09-18 15:25:33 +0100

    Sites duration report not working

--- web/WEB-INF/reports/57/script.py
+++ web/WEB-INF/reports/57/script.py
@@ -1,19 +1,21 @@
 from net.sf.chellow.monad import Monad
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import pytz
 
-Monad.getUtils()['imprt'](globals(), {
-        'db': ['Contract', 'Party', 'RateScript', 'set_read_write', 'session'], 
-        'utils': ['UserException'],
-        'templater': ['render']})
+Monad.getUtils()['impt'](globals(), 'db', 'utils', 'templater')
+
+HH = utils.HH
 
 sess = None
 try:
-    sess = session()
-    init = datetime.utcnow()
-    init = datetime(init.year, init.month, 1) - relativedelta(months=1)
+    sess = db.session()
+    now = datetime.now(pytz.utc)
+    month_start = datetime(now.year, now.month, 1) - relativedelta(months=1)
+    month_finish = month_start + relativedelta(months=1) - HH
 
-    render(inv, template, {'init': init})
+    templater.render(inv, template, {'month_start': month_start, 'month_finish': month_finish})
 
 finally:
     sess.close()
+
--- web/WEB-INF/reports/59/script.py
+++ web/WEB-INF/reports/59/script.py
@@ -1,111 +1,90 @@
-from net.sf.chellow.monad import Hiber, UserException
-from java.lang import System
-from net.sf.chellow.monad.types import MonadDate
-import math
-from java.sql import Timestamp, ResultSet
-from java.util import GregorianCalendar, Calendar, TimeZone, Locale, Date
-from net.sf.chellow.physical import HhStartDate
+from net.sf.chellow.monad import Monad
+import collections
+import operator
+import pytz
+import datetime
+from dateutil.relativedelta import relativedelta
+from sqlalchemy import or_, cast, Float
 
-year = inv.getInteger("year")
-month = inv.getInteger("month")
-months = inv.getInteger("months")
+Monad.getUtils()['impt'](globals(), 'templater', 'db', 'computer', 'utils')
 
-cal = GregorianCalendar(TimeZone.getTimeZone("GMT"), Locale.UK)
-cal.clear()
-cal.set(Calendar.YEAR, year)
-cal.set(Calendar.MONTH, month - 1)
-cal.add(Calendar.MONTH, 1)
-finish_date = HhStartDate(cal.getTime())
-cal.add(Calendar.MONTH, -1 * months)
-cal.add(Calendar.MINUTE, 30)
-start_date = HhStartDate(cal.getTime())
+HH, hh_before, hh_format = utils.HH, utils.hh_before, utils.hh_format
+form_date = utils.form_date
+Batch, Bill, Era, Site, SiteEra = db.Batch, db.Bill, db.Era, db.Site, db.SiteEra
+Supply, Source, Channel = db.Supply, db.Source, db.Channel
+HhDatum = db.HhDatum
+
+caches = {}
 
 inv.getResponse().setContentType("text/csv")
-inv.getResponse().setHeader('Content-Disposition', 'attachment; filename="output.csv"')
+inv.getResponse().setHeader('Content-Disposition', 'attachment; filename="sites_duration.csv"')
 pw = inv.getResponse().getWriter()
-pw.println("Site Id, Site Name, Associated Site Ids, Sources, Generator Types, From, To, Imported kWh, Displaced kWh, Exported kWh, Used kWh, Parasitic kWh, Generated kWh,Meter Type")
-pw.flush()
-sites = Hiber.session().createQuery("from Site site order by site.code").scroll()
-while sites.next():
-    query = Hiber.session().createQuery("select sum(datum.value) from HhDatum datum where datum.channel.era = :era and datum.channel.isImport is :isImport and datum.channel.isKwh is true and datum.channel.era.supply.source.code in (:sources) and datum.startDate.date >= :startDate and datum.startDate <= :finishDate")
-    site = sites.get(0)
-    site_code = site.getCode()
-    associates = []
-    sources = []
-    generator_types = []
-    has_physical = False
-    site_supply_eras = Hiber.session().createQuery("from SiteEra siteEra where siteEra.site = :site and siteEra.isPhysical is true and siteEra.era.supply.source.code != 'sub' and siteEra.era.startDate.date <= :finishDate and (siteEra.era.finishDate is null or siteEra.era.finishDate.date >= :startDate)").setEntity('site', site).setTimestamp('startDate', start_date.getDate()).setTimestamp('finishDate', finish_date.getDate()).scroll()
-    import_from_net = 0
-    export_to_net = 0
-    import_from_gen = 0
-    export_to_gen = 0
-    metering_type = 'nhh'
-    while site_supply_eras.next():
-        has_physical = True
-        site_supply_era = site_supply_eras.get(0)
-        supply_era = site_supply_era.getEra()
-        if metering_type == 'nhh' and supply_era.getChannels().size() > 0:
-            metering_type = 'amr'
-        if supply_era.getPc().getCode() == 0:
-            metering_type = 'hh'
-        for ss_gen in supply_era.getSiteEras():
-            ss_gen_site_code = ss_gen.getSite().getCode()
-            if ss_gen.getId() != site_supply_era.getId() and ss_gen_site_code != site_code and ss_gen_site_code not in associates:
-                associates.append(ss_gen_site_code)
-            sup = ss_gen.getEra().getSupply()
-            ss_gen_source = sup.getSource().getCode()
-            if ss_gen_source not in sources:
-                sources.append(ss_gen_source)
-            if sup.getGeneratorType() is not None:
-                ss_gen_gtype = sup.getGeneratorType().getCode()
-                if sup.getGeneratorType() not in generator_types:
-                    generator_types.append(ss_gen_gtype)
-        result = query.setEntity('era', supply_era).setTimestamp('startDate', start_date.getDate()).setTimestamp('finishDate', finish_date.getDate()).setBoolean('isImport', True).setParameterList('sources', ['net', 'gen-net']).uniqueResult()
-        if result is not None:
-            import_from_net = import_from_net + result.doubleValue()
-        result = query.setBoolean('isImport', False).uniqueResult()
-        if result is not None:
-            export_to_net = export_to_net + result.doubleValue()
-        result = query.setParameterList('sources', ['gen']).uniqueResult()
-        if result is not None:
-            export_to_gen = export_to_gen + result.doubleValue()
-        result = query.setParameterList('sources', ['gen-net']).uniqueResult()
-        if result is not None:
-            import_from_gen = import_from_gen + result.doubleValue()
-        result = query.setBoolean('isImport', True).uniqueResult()
-        if result is not None:
-            export_to_gen = export_to_gen + result.doubleValue()
-        result = query.setParameterList('sources', ['gen']).uniqueResult()
-        if result is not None:
-            import_from_gen = import_from_gen + result.doubleValue()
-    if has_physical:
-        associate_str = ''
-        for associate in associates:
-            associate_str = associate_str + associate + ', '
-        sources.sort()
-        sources_str = ''
-        for source in sources:
-            sources_str = sources_str + source + ', '
-        generator_types.sort()
-        generator_types_str = ''
-        for generator_type in generator_types:
-            generator_types_str = generator_types_str + generator_type + ', '
-        displaced = import_from_gen - export_to_net - export_to_gen
-        used = displaced + import_from_net
-        pw.print('"' + site_code + '","')
-        pw.print(site.getName() + '","')     
-        pw.print(associate_str[:-2] + '","')
-        pw.print(sources_str[:-2] + '","')
-        pw.print(generator_types_str[:-2] + '",')
-        pw.print(start_date.toString() + ',')
-        pw.print(finish_date.toString() + ',')
-        pw.print(str(round(import_from_net)))
-        pw.print(',' + str(round(displaced)) + ',')
-        pw.print(str(round(export_to_net)) + ',')
-        pw.print(str(round(used)) + ',')
-        pw.print(str(round(export_to_gen)) + ',')
-        pw.print(str(round(import_from_gen)) + ',' + metering_type + '\n')
+
+forecast_date = datetime.datetime.max.replace(tzinfo=pytz.utc)
+
+sess = None
+try:
+    sess = db.session()
+    pw.println("Site Id, Site Name, Associated Site Ids, Sources, Generator Types, From, To, Imported kWh, Displaced kWh, Exported kWh, Used kWh, Parasitic kWh, Generated kWh,Meter Type")
     pw.flush()
-    Hiber.session().clear()
-sites.close()
-pw.close()
+
+    start_date = form_date(inv, 'start')
+    finish_date = form_date(inv, 'finish')
+
+    streams = ('imp_net', 'displaced', 'exp_net', 'used', 'exp_gen', 'imp_gen')
+
+
+    sites = sess.query(Site).order_by(Site.code)
+    if inv.hasParameter('site_id'):
+        site_id = inv.getLong('site_id')
+        sites = sites.filter(Site.id == site_id)
+        
+    for site in sites:
+        sources = set()
+        generator_types = set()
+        assoc = set()
+
+        totals = dict((stream, 0) for stream in streams)
+
+        metering_type = 'nhh'
+        site_code = site.code
+
+        for group in site.groups(sess, start_date, finish_date, False):
+            assoc.update(s.code for s in group.sites if s.code != site_code)
+
+
+            for supply in group.supplies:
+                for era in supply.find_eras(sess, group.start_date, group.finish_date):
+                    if metering_type != 'hh':
+                        if era.pc.code == '00':
+                            metering_type = 'hh'
+                        elif metering_type != 'amr':
+                            if len(era.channels) > 0:
+                                metering_type = 'amr'
+                            elif metering_type != 'nhh':
+                                if era.mtc.meter_type.code in ['UM', 'PH']:
+                                    metering_type = 'unmetered'
+                                else:
+                                    metering_type = 'nhh'
+
+        for group in site.groups(sess, start_date, finish_date, True):
+            for supply in group.supplies:
+                sources.add(supply.source.code)
+                generator_type = supply.generator_type
+                if generator_type is not None:
+                    generator_types.add(generator_type.code)
+
+            for hh in group.hh_data(sess):
+                for stream in streams:
+                    totals[stream] += hh[stream]
+
+        assoc_str = ','.join(sorted(list(assoc)))
+        sources_str = ','.join(sorted(list(sources)))
+        generators_str = ','.join(sorted(list(generator_types)))
+        pw.println(','.join('"' + str(v) + '"' for v in (site.code, site.name, assoc_str, sources_str, generators_str, start_date.strftime("%Y-%m-%d %H:%M"), finish_date.strftime("%Y-%m-%d %H:%M"), totals['imp_net'], totals['displaced'], totals['exp_net'], totals['used'], totals['exp_gen'], totals['imp_gen'], metering_type)))
+        pw.flush()
+    pw.close()
+finally:
+    if sess is not None:
+        sess.close()
+
