10ef4c6e98f5d62c189affa73385ec19621feff3
Tony Locke <tony.locke@wessexwater.co.uk>
2014-09-25 14:42:13 +0100

    Show which contract fails on startup

--- web/WEB-INF/non_core_contracts/startup/charge_script.py
+++ web/WEB-INF/non_core_contracts/startup/charge_script.py
@@ -1,3 +1,4 @@
+import sys
 from net.sf.chellow.billing import Contract
 from net.sf.chellow.monad import Hiber, Monad
 import collections
@@ -56,15 +57,22 @@ def on_start_up(ctx):
 
     for contract_name in ['utils', 'pre_db', 'db', 'templater', 'bsuos',
             'tlms', 'general_import', 'hh_importer', 'bill_import', 'edi_lib', 'system_price_bmreports', 'system_price_elexon', 'system_price', 'rcrc', 'tlms', 'computer', 'duos', 'triad_rates', 'triad', 'ccl', 'aahedc']:
-        contract = Contract.getNonCoreContract(contract_name)
-        nspace = LibDict()
-        exec(contract.getChargeScript(), nspace)
-        for k, v in nspace.iteritems():
-            if not hasattr(nspace, k):
-                setattr(nspace, k, v)
-        nspace['db_id'] = contract.id
-        setattr(nspace, 'db_id', contract.id)
-        ctx.setAttribute("net.sf.chellow." + contract_name, nspace)
+        try:
+            contract = Contract.getNonCoreContract(contract_name)
+            nspace = LibDict()
+            exec(contract.getChargeScript(), nspace)
+            for k, v in nspace.iteritems():
+                if not hasattr(nspace, k):
+                    setattr(nspace, k, v)
+            nspace['db_id'] = contract.id
+            setattr(nspace, 'db_id', contract.id)
+            ctx.setAttribute(
+                "net.sf.chellow." + contract_name, nspace)
+        except:
+            cont = "\n Problem with contract " + contract_name
+            sys.stderr.write(cont)
+            System.err.write(cont)
+            raise
         
     download_path = Monad.getContext().getRealPath("/downloads")
     if not os.path.exists(download_path):
