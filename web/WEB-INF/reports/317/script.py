157b6755160528f2450b2ce1a6cfdc4e535d4d5b
Tony Locke <tony.locke@wessexwater.co.uk>
2014-10-08 15:52:36 +0100

    Can't update supplier contract properties

--- web/WEB-INF/reports/317/script.py
+++ web/WEB-INF/reports/317/script.py
@@ -8,7 +8,7 @@ Monad.getUtils()['imprt'](globals(), {
         'templater': ['render']})
 
 def make_fields(sess, contract, message=None):
-    parties = sess.query(Party).join(MarketRole, Participant).filter(MarketRole.code=='X').order_by(Participant.code).all()
+    parties = sess.query(Party).join(MarketRole, Participant).filter(MarketRole.code == 'X').order_by(Participant.code).all()
     messages = None if message is None else [str(message)]
     return {'contract': contract, 'messages': messages, 'parties': parties}
 
@@ -33,7 +33,8 @@ try:
             party = Party.get_by_id(sess, party_id)
             name = inv.getString('name')
             charge_script = inv.getString('charge_script')
-            contract.update(sess, False, name, party, charge_script, '{}')
+            properties = inv.getString('properties')
+            contract.update(sess, False, name, party, charge_script, properties)
             sess.commit()
             inv.sendSeeOther('/reports/77/output/?supplier_contract_id='
                 + str(contract.id))
