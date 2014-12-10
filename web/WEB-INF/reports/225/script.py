19c2cf7814587acc415fc3d5b3b88561ac009907
Tony Locke <tony.locke@wessexwater.co.uk>
2014-12-10 16:58:52 +0000

    RCRC automatic importer page failing

--- web/WEB-INF/reports/225/script.py
+++ web/WEB-INF/reports/225/script.py
@@ -1,31 +1,32 @@
 from net.sf.chellow.monad import Monad
-from java.lang import System
-from sqlalchemy.orm import joinedload_all
-import sys
-
-Monad.getContext().getAttribute("net.sf.chellow.utils")['imprt'](globals(), {
-        'db': ['Contract', 'Party', 'RateScript', 'set_read_write', 'session'], 
-        'utils': ['UserException'],
-        'templater': ['render'],
-        'rcrc': ['get_rcrc_importer']})
-
+import db
+import rcrc
+import templater
+import utils
+Monad.getUtils()['impt'](globals(), 'db', 'utils', 'templater', 'rcrc')
+Contract = db.Contract
+render = templater.render
+UserException = utils.UserException
+inv, template = globals()['inv'], globals()['template']
 
 sess = None
 importer = None
 try:
-    sess = session()
+    sess = db.session()
     if inv.getRequest().getMethod() == "GET":
-        importer = get_rcrc_importer()
+        importer = rcrc.get_rcrc_importer()
         contract = Contract.get_non_core_by_name(sess, 'rcrc')
         render(inv, template, {'importer': importer, 'contract': contract})
     else:
-        importer = get_rcrc_importer()
+        importer = rcrc.get_rcrc_importer()
         contract = Contract.get_non_core_by_name(sess, 'rcrc')
         importer.go()
         inv.sendSeeOther("/reports/225/output/")
 except UserException, e:
     sess.rollback()
-    render(inv, template, {'messages': [str(e)], 'importer': importer,
-        'contract': contract})
+    render(
+        inv, template, {
+            'messages': [str(e)], 'importer': importer, 'contract': contract})
 finally:
-    sess.close()
+    if sess is not None:
+        sess.close()
