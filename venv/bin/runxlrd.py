61df9e3c32a4944a4edbd4be0d447e7746399789
Juvi Andaya <juvilyn.andaya@wessexwater.co.uk>
2016-06-10 14:44:37 +0800

    Update Base.html and Home.html to fix test errors

---
+++ venv/bin/runxlrd.py
@@ -0,0 +1,4 @@
+#!/home/JuviAndaya/chellow/venv/bin/python
+# EASY-INSTALL-SCRIPT: 'xlrd==0.9.4','runxlrd.py'
+__requires__ = 'xlrd==0.9.4'
+__import__('pkg_resources').run_script('xlrd==0.9.4', 'runxlrd.py')
