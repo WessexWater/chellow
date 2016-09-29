57f9432529e6646e308307a60485eee5dc12b2e6
Juvi Andaya <juvilyn.andaya@wessexwater.co.uk>
2016-09-29 16:00:17 +0800

    Change background to configurable online

--- chellow/views.py
+++ chellow/views.py
@@ -262,64 +262,14 @@ def style_get():
     response.headers['Content-type'] = 'text/css'
     return response
 
-@app.route('/bootstrap', methods=['GET'])
-def bootstrap_get():
-    props = Contract.get_non_core_by_name(g.sess, 'configuration'). \
-        make_properties()
-    response = make_response(
-        render_template(
-            'css/bootstrap.min.css', background_colour=props['background_colour']))
-    response.headers['Content-type'] = 'text/css'
-    return response
-
-
-@app.route('/wessexcss', methods=['GET'])
-def wessexcss_get():
-    props = Contract.get_non_core_by_name(g.sess, 'configuration'). \
-        make_properties()
-    response = make_response(
-        render_template(
-            'css/wessex.css', background_colour=props['background_colour']))
-    response.headers['Content-type'] = 'text/css'
-    return response
-
-@app.route('/bootstrapjs', methods=['GET'])
-def bootstrapjs_get():
-    props = Contract.get_non_core_by_name(g.sess, 'configuration'). \
-        make_properties()
-    response = make_response(
-        render_template(
-            'js/bootstrap.min.js', background_colour=props['background_colour']))
-    response.headers['Content-type'] = 'text/javascript'
-    return response
-
-@app.route('/gridsjs', methods=['GET'])
-def gridjs_get():
-    props = Contract.get_non_core_by_name(g.sess, 'configuration'). \
-        make_properties()
-    response = make_response(
-        render_template(
-            'js/grids.min.js', background_colour=props['background_colour']))
-    response.headers['Content-type'] = 'text/javascript'
-    return response
-
-@app.route('/wessexjs', methods=['GET'])
-def wessexjs_get():
-    props = Contract.get_non_core_by_name(g.sess, 'configuration'). \
-        make_properties()
-    response = make_response(
-        render_template(
-            'js/wessex.js', background_colour=props['background_colour']))
-    response.headers['Content-type'] = 'text/javascript'
-    return response
-
-
 
 @app.route('/chellowcss', methods=['GET'])
 def chellowcss_get():
+    props = Contract.get_non_core_by_name(g.sess, 'configuration'). \
+        make_properties()
     response = make_response(
         render_template(
-            'css/chellow.css'))
+            'css/chellow.css', background_colour=props['background_colour']))
     response.headers['Content-type'] = 'text/css'
     return response
 
