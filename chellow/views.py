e64f731c046175cbdb08263decbe3dff3e670185
Tony Locke <tony.locke@wessexwater.co.uk>
2017-04-24 10:21:25 +0100

    Spead up supply months report

--- chellow/views.py
+++ chellow/views.py
@@ -1604,48 +1604,43 @@ def supply_months_get(supply_id):
                 month_data['sc'] = era.imp_sc if is_import else era.exp_sc
 
         md_kvah = 0
+        channel_ids = [
+            c.id for c in g.sess.query(Channel).join(Era).filter(
+                Era.supply == supply, Era.start_date <= month_finish,
+                or_(
+                    Era.finish_date == null(), Era.finish_date >= month_start),
+                Channel.imp_related == is_import)]
         for kwh, kvarh, hh_date in g.sess.execute(
-                "select cast(max(hh_ac.value) as double precision), "
-                "cast(max(hh_re.value) as double precision), "
-                "hh_ac.start_date "
-                "from hh_datum as hh_ac join channel as channel_ac "
-                "on (hh_ac.channel_id = channel_ac.id) "
-                "join era as era_ac on (channel_ac.era_id = era_ac.id) "
-                "join hh_datum as hh_re "
-                "on (hh_ac.start_date = hh_re.start_date) "
-                "join channel as channel_re "
-                "on (hh_re.channel_id = channel_re.id) "
-                "join era as era_re on (channel_re.era_id = era_re.id) "
-                "where era_ac.supply_id = :supply_id "
-                "and era_re.supply_id = :supply_id "
-                "and channel_ac.imp_related = :is_import "
-                "and channel_re.imp_related = :is_import "
-                "and channel_ac.channel_type = 'ACTIVE' "
-                "and channel_re.channel_type "
-                "in ('REACTIVE_IMP', 'REACTIVE_EXP') "
-                "and hh_ac.start_date >= :month_start "
-                "and hh_ac.start_date <= :month_finish "
-                "and hh_re.start_date >= :month_start "
-                "and hh_re.start_date <= :month_finish "
-                "group by hh_ac.start_date",
+                "select cast(max("
+                "case when channel.channel_type = 'ACTIVE' then "
+                "hh_datum.value else 0 end) as double precision), "
+                "cast(max(case when channel.channel_type in ("
+                "'REACTIVE_IMP', 'REACTIVE_EXP') then hh_datum.value else 0 "
+                "end) as double precision), "
+                "hh_datum.start_date "
+                "from hh_datum join channel "
+                "on (hh_datum.channel_id = channel.id) "
+                "where channel.id = any(:channel_ids) and "
+                "hh_datum.start_date >= :month_start "
+                "and hh_datum.start_date <= :month_finish "
+                "group by hh_datum.start_date",
                 params={
-                    'month_start': month_start, 'month_finish': month_finish,
-                    'is_import': is_import, 'supply_id': supply.id}):
+                    'channel_ids': channel_ids, 'month_start': month_start,
+                    'month_finish': month_finish}):
             kvah = (kwh ** 2 + kvarh ** 2) ** 0.5
             if kvah > md_kvah:
                 md_kvah = kvah
                 month_data['md_kva'] = 2 * md_kvah
                 month_data['md_kvar'] = kvarh * 2
                 month_data['md_kw'] = kwh * 2
-                month_data['md_pf'] = float(kwh) / kvah
+                month_data['md_pf'] = kwh / kvah
                 month_data['md_date'] = hh_date
 
         total_kwh = g.sess.query(func.sum(HhDatum.value)).join(Channel) \
-            .join(Era).filter(
-                Era.supply == supply, Channel.channel_type == 'ACTIVE',
-                Channel.imp_related == is_import,
+            .filter(
+                Channel.id.in_(channel_ids), Channel.channel_type == 'ACTIVE',
                 HhDatum.start_date >= month_start,
-                HhDatum.start_date <= month_finish).one()[0]
+                HhDatum.start_date <= month_finish).scalar()
 
         if total_kwh is not None:
             month_data['total_kwh'] = float(total_kwh)
