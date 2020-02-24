9b23489f3af0923584a461542841c07a29b37e57
Tony Locke <tlocke@tlocke.org.uk>
2020-02-24 09:51:29 +0000

    Speed up register read report

--- chellow/reports/report_219.py
+++ chellow/reports/report_219.py
@@ -1,8 +1,9 @@
-from dateutil.relativedelta import relativedelta
 from sqlalchemy import or_, and_
+from sqlalchemy.orm import joinedload
 import traceback
-from chellow.models import RegisterRead, Bill, Supply, Era, Session
-from chellow.utils import HH, hh_format, req_int, utc_datetime
+from chellow.models import (
+    RegisterRead, Bill, Supply, Era, Session, Batch, BillType)
+from chellow.utils import csv_make_val, c_months_u, req_int
 from flask import request, g
 import chellow.dloads
 import csv
@@ -20,64 +21,83 @@ def content(year, month, months, supply_id, user):
             'register_reads.csv', user)
         f = open(running_name, mode='w', newline='')
         w = csv.writer(f, lineterminator='\n')
-        w.writerow(
-            (
-                'Duration Start', 'Duration Finish', 'Supply Id',
-                'Import MPAN Core', 'Export MPAN Core', 'Batch Reference',
-                'Bill Id', 'Bill Reference', 'Bill Issue Date', 'Bill Type',
-                'Register Read Id', 'TPR', 'Coefficient',
-                'Previous Read Date', 'Previous Read Value',
-                'Previous Read Type', 'Present Read Date',
-                'Present Read Value', 'Present Read Type'))
+        titles = (
+            'Duration Start', 'Duration Finish', 'Supply Id',
+            'Import MPAN Core', 'Export MPAN Core', 'Batch Reference',
+            'Bill Id', 'Bill Reference', 'Bill Issue Date', 'Bill Type',
+            'Register Read Id', 'TPR', 'Coefficient', 'Previous Read Date',
+            'Previous Read Value', 'Previous Read Type', 'Present Read Date',
+            'Present Read Value', 'Present Read Type'
+        )
+        w.writerow(titles)
 
-        finish_date = utc_datetime(year, month, 1) + \
-            relativedelta(months=1) - HH
+        month_pairs = list(
+            c_months_u(finish_year=year, finish_month=month, months=months))
+        start_date, finish_date = month_pairs[0][0], month_pairs[-1][-1]
 
-        start_date = utc_datetime(year, month, 1) - \
-            relativedelta(months=months-1)
-
-        reads = sess.query(RegisterRead).filter(
+        supplies = sess.query(Supply).join(Bill).join(RegisterRead).filter(
             or_(
                 and_(
                     RegisterRead.present_date >= start_date,
                     RegisterRead.present_date <= finish_date),
                 and_(
                     RegisterRead.previous_date >= start_date,
-                    RegisterRead.previous_date <= finish_date))) \
-            .join(Bill).order_by(Bill.supply_id)
+                    RegisterRead.previous_date <= finish_date))).order_by(
+            Bill.supply_id).distinct()
 
         if supply_id is not None:
             supply = Supply.get_by_id(sess, supply_id)
-            reads = reads.filter(Bill.supply == supply)
+            supplies = supplies.filter(Bill.supply == supply)
 
-        for read in reads:
-            bill = read.bill
-            supply = bill.supply
-            batch = bill.batch
+        for supply in supplies:
+            supply_id = supply.id
+            for bill, batch, bill_type in sess.query(
+                    Bill, Batch, BillType).join(Batch).join(BillType).join(
+                    RegisterRead).filter(
+                    Bill.supply == supply, or_(
+                        and_(
+                            RegisterRead.present_date >= start_date,
+                            RegisterRead.present_date <= finish_date),
+                        and_(
+                            RegisterRead.previous_date >= start_date,
+                            RegisterRead.previous_date <= finish_date))):
 
-            era = supply.find_era_at(sess, bill.start_date)
-            if era is None:
-                eras = sess.query(Era).filter(
-                    Era.supply == supply).order_by(Era.start_date).all()
-                if bill.start_date < eras[0].start_date:
-                    era = eras[0]
-                else:
-                    era = eras[-1]
+                era = supply.find_era_at(sess, bill.start_date)
+                if era is None:
+                    eras = sess.query(Era).filter(
+                        Era.supply == supply).order_by(Era.start_date).all()
+                    if bill.start_date < eras[0].start_date:
+                        era = eras[0]
+                    else:
+                        era = eras[-1]
 
-            w.writerow(
-                ('' if val is None else val) for val in [
-                    hh_format(start_date), hh_format(finish_date), supply.id,
-                    era.imp_mpan_core, era.exp_mpan_core, batch.reference,
-                    bill.id, bill.reference, hh_format(bill.issue_date),
-                    bill.bill_type.code, read.id,
-                    'md' if read.tpr is None else read.tpr.code,
-                    read.coefficient, hh_format(read.previous_date),
-                    read.previous_value, read.previous_type.code,
-                    hh_format(read.present_date), read.present_value,
-                    read.present_type.code])
+                for read in sess.query(RegisterRead).filter(
+                        RegisterRead.bill == bill, or_(
+                            and_(
+                                RegisterRead.present_date >= start_date,
+                                RegisterRead.present_date <= finish_date),
+                            and_(
+                                RegisterRead.previous_date >= start_date,
+                                RegisterRead.previous_date <= finish_date))
+                        ).options(
+                            joinedload(RegisterRead.tpr),
+                            joinedload(RegisterRead.previous_type),
+                            joinedload(RegisterRead.present_type)):
 
-            # Avoid a long-running transaction
-            sess.rollback()
+                    vals = [
+                        start_date, finish_date, supply_id, era.imp_mpan_core,
+                        era.exp_mpan_core, batch.reference, bill.id,
+                        bill.reference, bill.issue_date, bill_type.code,
+                        read.id, 'md' if read.tpr is None else read.tpr.code,
+                        read.coefficient, read.previous_date,
+                        read.previous_value, read.previous_type.code,
+                        read.present_date, read.present_value,
+                        read.present_type.code
+                    ]
+                    w.writerow(csv_make_val(v) for v in vals)
+
+                # Avoid a long-running transaction
+                sess.rollback()
 
     except BadRequest as e:
         w.writerow([e.description])
