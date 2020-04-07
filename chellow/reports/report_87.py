b111d02e51acc9e54ead2d1d2da2aa45794572ca
Tony Locke <tlocke@tlocke.org.uk>
2020-04-07 12:06:04 +0100

    Test for contract virtual bill

--- chellow/reports/report_87.py
+++ chellow/reports/report_87.py
@@ -16,102 +16,114 @@ import threading
 from flask import g
 
 
-def content(start_date, finish_date, contract_id, user):
+def create_csv(f, sess, start_date, finish_date, contract_id):
     caches = {}
+    writer = csv.writer(f, lineterminator='\n')
+    contract = Contract.get_supplier_by_id(sess, contract_id)
+    forecast_date = chellow.computer.forecast_date()
+
+    start_date_ct, finish_date_ct = to_ct(start_date), to_ct(finish_date)
+
+    month_pairs = c_months_u(
+        start_year=start_date_ct.year, start_month=start_date_ct.month,
+        finish_year=finish_date_ct.year, finish_month=finish_date_ct.month)
+
+    bill_titles = contract_func(caches, contract, 'virtual_bill_titles')()
+
+    for tpr in sess.query(Tpr).join(MeasurementRequirement).join(Ssc). \
+            join(Era).filter(
+                Era.start_date <= finish_date, or_(
+                    Era.finish_date == null(),
+                    Era.finish_date >= start_date), or_(
+                    Era.imp_supplier_contract == contract,
+                    Era.exp_supplier_contract == contract)
+            ).order_by(Tpr.code).distinct():
+        for suffix in ('-kwh', '-rate', '-gbp'):
+            bill_titles.append(tpr.code + suffix)
+    writer.writerow(
+        ['MPAN Core', 'Site Code', 'Site Name', 'Account', 'From', 'To'] +
+        bill_titles)
+    vb_func = contract_func(caches, contract, 'virtual_bill')
+
+    for month_start, month_finish in month_pairs:
+        period_start = hh_max(start_date, month_start)
+        period_finish = hh_min(finish_date, month_finish)
+
+        for era in sess.query(Era).distinct().filter(
+                or_(
+                    Era.imp_supplier_contract == contract,
+                    Era.exp_supplier_contract == contract),
+                Era.start_date <= period_finish,
+                or_(
+                    Era.finish_date == null(),
+                    Era.finish_date >= period_start)):
+
+            chunk_start = hh_max(period_start, era.start_date)
+            chunk_finish = hh_min(period_finish, era.finish_date)
+
+            polarities = []
+            if era.imp_supplier_contract == contract:
+                polarities.append(True)
+            if era.exp_supplier_contract == contract:
+                polarities.append(False)
+            for polarity in polarities:
+                vals = []
+                data_source = SupplySource(
+                    sess, chunk_start, chunk_finish, forecast_date, era,
+                    polarity, caches)
+
+                site = sess.query(Site).join(SiteEra).filter(
+                    SiteEra.era == era, SiteEra.is_physical == true()).one()
+
+                vals = [
+                    data_source.mpan_core, site.code, site.name,
+                    data_source.supplier_account, data_source.start_date,
+                    data_source.finish_date
+                ]
+
+                vb_func(data_source)
+                bill = data_source.supplier_bill
+                for title in bill_titles:
+                    if title in bill:
+                        val = bill[title]
+                        del bill[title]
+                    else:
+                        val = ''
+                    vals.append(val)
+
+                for k in sorted(bill.keys()):
+                    vals.append(k)
+                    vals.append(str(bill[k]))
+                writer.writerow(csv_make_val(v) for v in vals)
+
+
+def content(start_date, finish_date, contract_id, user):
     sess = f = None
     try:
-        sess = Session()
         running_name, finished_name = chellow.dloads.make_names(
             'virtual_bills.csv', user)
 
         f = open(running_name, mode='w', newline='')
-        writer = csv.writer(f, lineterminator='\n')
-        contract = Contract.get_supplier_by_id(sess, contract_id)
-        forecast_date = chellow.computer.forecast_date()
-        start_date_ct, finish_date_ct = to_ct(start_date), to_ct(finish_date)
+        sess = Session()
 
-        month_pairs = c_months_u(
-            start_year=start_date_ct.year, start_month=start_date_ct.month,
-            finish_year=finish_date_ct.year, finish_month=finish_date_ct.month)
-
-        bill_titles = contract_func(caches, contract, 'virtual_bill_titles')()
-
-        for tpr in sess.query(Tpr).join(MeasurementRequirement).join(Ssc). \
-                join(Era).filter(
-                    Era.start_date <= finish_date, or_(
-                        Era.finish_date == null(),
-                        Era.finish_date >= start_date), or_(
-                        Era.imp_supplier_contract == contract,
-                        Era.exp_supplier_contract == contract)
-                ).order_by(Tpr.code).distinct():
-            for suffix in ('-kwh', '-rate', '-gbp'):
-                bill_titles.append(tpr.code + suffix)
-        writer.writerow(
-            ['MPAN Core', 'Site Code', 'Site Name', 'Account', 'From', 'To'] +
-            bill_titles)
-        vb_func = contract_func(caches, contract, 'virtual_bill')
-
-        for month_start, month_finish in month_pairs:
-            period_start = hh_max(start_date, month_start)
-            period_finish = hh_min(finish_date, month_finish)
-
-            for era in sess.query(Era).distinct().filter(
-                    or_(
-                        Era.imp_supplier_contract == contract,
-                        Era.exp_supplier_contract == contract),
-                    Era.start_date <= period_finish,
-                    or_(
-                        Era.finish_date == null(),
-                        Era.finish_date >= period_start)):
-
-                chunk_start = hh_max(period_start, era.start_date)
-                chunk_finish = hh_min(period_finish, era.finish_date)
-
-                polarities = []
-                if era.imp_supplier_contract == contract:
-                    polarities.append(True)
-                if era.exp_supplier_contract == contract:
-                    polarities.append(False)
-                for polarity in polarities:
-                    vals = []
-                    data_source = SupplySource(
-                        sess, chunk_start, chunk_finish, forecast_date, era,
-                        polarity, caches)
-
-                    site = sess.query(Site).join(SiteEra).filter(
-                        SiteEra.era == era,
-                        SiteEra.is_physical == true()).one()
-
-                    vals = [
-                        data_source.mpan_core, site.code, site.name,
-                        data_source.supplier_account, data_source.start_date,
-                        data_source.finish_date
-                    ]
-
-                    vb_func(data_source)
-                    bill = data_source.supplier_bill
-                    for title in bill_titles:
-                        if title in bill:
-                            val = bill[title]
-                            del bill[title]
-                        else:
-                            val = ''
-                        vals.append(val)
-
-                    for k in sorted(bill.keys()):
-                        vals.append(k)
-                        vals.append(str(bill[k]))
-                    writer.writerow(csv_make_val(v) for v in vals)
+        create_csv(f, sess, start_date, finish_date, contract_id)
 
     except BadRequest as e:
-        writer.writerow([e.description])
+        if f is None:
+            raise
+        else:
+            f.write('\n' + e.description)
     except BaseException:
-        writer.writerow([traceback.format_exc()])
+        if f is None:
+            raise
+        else:
+            f.write(traceback.format_exc())
     finally:
         if sess is not None:
             sess.close()
-        f.close()
-        os.rename(running_name, finished_name)
+        if f is not None:
+            f.close()
+            os.rename(running_name, finished_name)
 
 
 def do_get(sess):
---
+++ test/pytest/test_report_87.py
@@ -0,0 +1,61 @@
+from chellow.utils import ct_datetime, to_utc
+import chellow.reports.report_87
+import chellow.computer
+from io import StringIO
+
+
+class Sess():
+    def __init__(self, *results):
+        self.it = iter(results)
+
+    def query(self, *arg):
+        return self
+
+    def join(self, *arg):
+        return self
+
+    def order_by(self, *arg):
+        return self
+
+    def filter(self, *arg):
+        return self
+
+    def scalar(self, *arg):
+        return next(self.it)
+
+    def first(self, *arg):
+        return next(self.it)
+
+    def distinct(self, *arg):
+        return next(self.it)
+
+
+def test_summertime(mocker):
+    start_date_ct = ct_datetime(2010, 5)
+    finish_date_ct = ct_datetime(2010, 5, 31, 23, 30)
+    start_date = to_utc(start_date_ct)
+    finish_date = to_utc(finish_date_ct)
+    contract_id = 1
+    user = mocker.Mock()
+    user.email_address = "sfreud"
+
+    mock_c_months_u = mocker.patch(
+        'chellow.reports.report_87.c_months_u', autospec=True)
+
+    MockEra = mocker.patch('chellow.reports.report_87.Era', autospec=True)
+    MockEra.finish_date = finish_date
+    MockEra.start_date = start_date
+
+    mocker.patch(
+        'chellow.reports.report_87.contract_func', autospec=True,
+        return_value=lambda: ['standing_gbp'])
+
+    f = StringIO()
+    sess = Sess([], [])
+
+    chellow.reports.report_87.create_csv(
+        f, sess, start_date, finish_date, contract_id)
+
+    mock_c_months_u.assert_called_with(
+        start_year=start_date_ct.year, start_month=start_date_ct.month,
+        finish_year=finish_date_ct.year, finish_month=finish_date_ct.month)
