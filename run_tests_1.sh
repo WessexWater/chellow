d11292c740e8bb3117bec377643e0ff315add382
Tony Locke <tlocke@tlocke.org.uk>
2020-10-09 11:37:49 +0100

    Added blank rate file for DNO 38

--- run_tests_1.sh
+++ run_tests_1.sh
@@ -1,4 +1,5 @@
 export NO_PROXY=localhost
+rm -r instance/downloads
 dropdb --if-exists toxchellow
 createdb --encoding=UTF8 toxchellow
 waitress-serve --host=0.0.0.0 --port=8080 --call chellow:create_app
