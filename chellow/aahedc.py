7b9ce0e69a9297ab04217b49cb6e7126fa473cd7
Tony Locke <tlocke@tlocke.org.uk>
2022-07-12 16:40:22 +0100

    aahedc doesn't work

--- chellow/aahedc.py
+++ chellow/aahedc.py
@@ -15,7 +15,7 @@ def hh(supply_source):
             hh_start = hh["start-date"]
             db_id = get_non_core_contract_id("aahedc")
             rates = supply_source.hh_rate(db_id, hh_start)
-            rate = aahedc_cache[hh_start] = rates["aahedc_gbp_per_gsp_kwh"]
+            rate = aahedc_cache[hh_start] = float(rates["aahedc_gbp_per_gsp_kwh"])
 
         hh["aahedc-rate"] = rate
         hh["aahedc-gbp"] = hh["gsp-kwh"] * rate
