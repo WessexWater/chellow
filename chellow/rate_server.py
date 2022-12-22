147f690dbe54e10e155b01bba7716f1bff5f74c6
Tony Locke <tlocke@tlocke.org.uk>
2022-12-22 14:13:47 +0000

    Allow configuration of rate server url

--- chellow/rate_server.py
+++ chellow/rate_server.py
@@ -22,6 +22,55 @@ def download(s, url):
     return b64decode(fl_json["content"])
 
 
+def run_import(sess, log):
+
+    log("Starting to import rates from the rate server")
+    conf = Contract.get_non_core_by_name(sess, "configuration")
+    props = conf.make_properties()
+    repo_props = props.get("rate_server", {})
+    repo_url = repo_props.get(
+        "url", "https://api.github.com/repos/WessexWater/chellow-rates"
+    )
+    repo_branch = repo_props.get("branch", "main")
+    s = requests.Session()
+    s.verify = False
+    repo_entry = s.get(repo_url).json()
+    if "message" in repo_entry:
+        raise Exception(f"Message from the GitHub API: {repo_entry['message']}")
+
+    log(
+        f"Looking at {repo_entry['html_url']} and branch "
+        f"{'default' if repo_branch is None else repo_branch}"
+    )
+    branch_entry = s.get(f"{repo_url}/branches/{repo_branch}").json()
+
+    tree_entry = s.get(
+        branch_entry["commit"]["commit"]["tree"]["url"],
+        params={"recursive": "true"},
+    ).json()
+
+    if tree_entry["truncated"]:
+        raise Exception("Tree from rate server is truncated.")
+
+    paths_list = []
+    for sub_entry in tree_entry["tree"]:
+        path = sub_entry["path"].split("/")
+        if path[-1] != "README.md":
+
+            paths_list.append((path, sub_entry["url"]))
+
+    paths = tuple(paths_list)
+
+    for mod_name in (
+        "chellow.e.dno_rate_parser",
+        "chellow.e.mdd_importer",
+        "chellow.e.tnuos",
+        "chellow.gas.dn_rate_parser",
+    ):
+        mod = import_module(mod_name)
+        mod.rate_server_import(sess, s, paths, log)
+
+
 class RateServer(threading.Thread):
     def __init__(self):
         super().__init__(name="Rate Server")
@@ -29,7 +78,6 @@ class RateServer(threading.Thread):
         self.messages = collections.deque(maxlen=500)
         self.stopped = threading.Event()
         self.going = threading.Event()
-        self.repo_url = "https://api.github.com/repos/WessexWater/chellow-rates"
 
     def stop(self):
         self.stopped.set()
@@ -56,52 +104,8 @@ class RateServer(threading.Thread):
             if self.lock.acquire(False):
                 sess = self.global_alert = None
                 try:
-                    self.log("Starting to import rates from the rate server")
                     sess = Session()
-                    conf = Contract.get_non_core_by_name(sess, "configuration")
-                    props = conf.make_properties()
-                    repo_branch = props.get("rate_server_branch", "main")
-                    s = requests.Session()
-                    s.verify = False
-                    repo_entry = s.get(self.repo_url).json()
-                    if "message" in repo_entry:
-                        raise Exception(
-                            f"Message from the GitHub API: {repo_entry['message']}"
-                        )
-
-                    self.log(
-                        f"Looking at {repo_entry['html_url']} and branch "
-                        f"{'default' if repo_branch is None else repo_branch}"
-                    )
-                    branch_entry = s.get(
-                        f"{self.repo_url}/branches/{repo_branch}"
-                    ).json()
-
-                    tree_entry = s.get(
-                        branch_entry["commit"]["commit"]["tree"]["url"],
-                        params={"recursive": "true"},
-                    ).json()
-
-                    if tree_entry["truncated"]:
-                        raise Exception("Tree from rate server is truncated.")
-
-                    paths_list = []
-                    for sub_entry in tree_entry["tree"]:
-                        path = sub_entry["path"].split("/")
-                        if path[-1] != "README.md":
-
-                            paths_list.append((path, sub_entry["url"]))
-
-                    paths = tuple(paths_list)
-
-                    for mod_name in (
-                        "chellow.e.dno_rate_parser",
-                        "chellow.e.mdd_importer",
-                        "chellow.e.tnuos",
-                        "chellow.gas.dn_rate_parser",
-                    ):
-                        mod = import_module(mod_name)
-                        mod.rate_server_import(sess, s, paths, self.log)
+                    run_import(sess, self.log)
                 except BaseException:
                     self.log(traceback.format_exc())
                     self.global_alert = "Rate Server: An import has failed"
