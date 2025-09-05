fbf25b7088cd95d6df5c459b357bba253ed82e46
Tony Locke <tlocke@tlocke.org.uk>
2025-09-05 09:56:40 +0100

    i

--- chellow/e/bill_importer.py
+++ chellow/e/bill_importer.py
@@ -75,97 +75,107 @@ class BillImport(threading.Thread):
 
     def run(self):
         try:
+            batch_ids = set()
             with Session() as sess:
-                bf_q = select(BatchFile).order_by(BatchFile.upload_timestamp)
+                batch_q = select(Batch)
                 if self.batch_id is not None:
                     batch = Batch.get_by_id(sess, self.batch_id)
-                    bf_q = bf_q.where(BatchFile.batch == batch)
+                    batch_q = batch_q.where(Batch.id == self.batch_id)
                 elif self.contract_id is not None:
                     contract = Contract.get_by_id(sess, self.contract_id)
-                    bf_q = bf_q.join(Batch).where(Batch.contract == contract)
-
-                bill_types = keydefaultdict(lambda k: BillType.get_by_code(sess, k))
-
-                tprs = keydefaultdict(
-                    lambda k: None if k is None else Tpr.get_by_code(sess, k)
-                )
-
-                read_types = keydefaultdict(lambda k: ReadType.get_by_code(sess, k))
-
-                for bf in sess.scalars(bf_q):
-                    self.parser = _process_batch_file(sess, bf, self._log)
-                    for self.bill_num, raw_bill in enumerate(
-                        self.parser.make_raw_bills()
-                    ):
-                        batch = bf.batch
-                        if "error" in raw_bill:
-                            self.failed_bills.append(raw_bill)
-                        else:
-                            try:
-                                mpan_core = raw_bill["mpan_core"]
-                                supply = Supply.get_by_mpan_core(sess, mpan_core)
-                                with sess.begin_nested():
-                                    bill = batch.insert_bill(
-                                        sess,
-                                        raw_bill["account"],
-                                        raw_bill["reference"],
-                                        raw_bill["issue_date"],
-                                        raw_bill["start_date"],
-                                        raw_bill["finish_date"],
-                                        raw_bill["kwh"],
-                                        raw_bill["net"],
-                                        raw_bill["vat"],
-                                        raw_bill["gross"],
-                                        bill_types[raw_bill["bill_type_code"]],
-                                        raw_bill["breakdown"],
-                                        supply,
-                                    )
-                                    for raw_element in raw_bill["elements"]:
-                                        bill.insert_element(
-                                            sess,
-                                            raw_element["name"],
-                                            raw_element["start_date"],
-                                            raw_element["finish_date"],
-                                            raw_element["net"],
-                                            raw_element["breakdown"],
-                                        )
-                                    for raw_read in raw_bill["reads"]:
-                                        bill.insert_read(
-                                            sess,
-                                            tprs[raw_read["tpr_code"]],
-                                            raw_read["coefficient"],
-                                            raw_read["units"],
-                                            raw_read["msn"],
-                                            raw_read["mpan"],
-                                            raw_read["prev_date"],
-                                            raw_read["prev_value"],
-                                            read_types[raw_read["prev_type_code"]],
-                                            raw_read["pres_date"],
-                                            raw_read["pres_value"],
-                                            read_types[raw_read["pres_type_code"]],
-                                        )
-                                    self.successful_bills.append(raw_bill)
-                            except KeyError as e:
-                                err = raw_bill.get("error", "")
-                                raw_bill["error"] = err + " " + str(e)
-                                self.failed_bills.append(raw_bill)
-                            except BadRequest as e:
-                                raw_bill["error"] = str(e.description)
-                                self.failed_bills.append(raw_bill)
-
-                if len(self.failed_bills) == 0:
-                    sess.commit()
-                    self._log(
-                        "All the bills have been successfully loaded and attached "
-                        "to the batch."
+                    batch_q = batch_q.where(Batch.contract == contract)
+                for batch in sess.scalars(batch_q):
+                    batch_ids.add(batch.id)
+            for batch_id in batch_ids:
+                with Session() as sess:
+                    bf_q = (
+                        select(BatchFile)
+                        .where(BatchFile.batch_id == batch_id)
+                        .order_by(BatchFile.upload_timestamp)
                     )
-                else:
-                    sess.rollback()
-                    self._log(
-                        f"The import has finished, but there were "
-                        f"{len(self.failed_bills)} "
-                        f"failures, and so the whole import has been rolled back."
+                    bill_types = keydefaultdict(lambda k: BillType.get_by_code(sess, k))
+
+                    tprs = keydefaultdict(
+                        lambda k: None if k is None else Tpr.get_by_code(sess, k)
                     )
+
+                    read_types = keydefaultdict(lambda k: ReadType.get_by_code(sess, k))
+
+                    for bf in sess.scalars(bf_q):
+                        self.parser = _process_batch_file(sess, bf, self._log)
+                        for self.bill_num, raw_bill in enumerate(
+                            self.parser.make_raw_bills()
+                        ):
+                            batch = bf.batch
+                            if "error" in raw_bill:
+                                self.failed_bills.append(raw_bill)
+                            else:
+                                try:
+                                    mpan_core = raw_bill["mpan_core"]
+                                    supply = Supply.get_by_mpan_core(sess, mpan_core)
+                                    with sess.begin_nested():
+                                        bill = batch.insert_bill(
+                                            sess,
+                                            raw_bill["account"],
+                                            raw_bill["reference"],
+                                            raw_bill["issue_date"],
+                                            raw_bill["start_date"],
+                                            raw_bill["finish_date"],
+                                            raw_bill["kwh"],
+                                            raw_bill["net"],
+                                            raw_bill["vat"],
+                                            raw_bill["gross"],
+                                            bill_types[raw_bill["bill_type_code"]],
+                                            raw_bill["breakdown"],
+                                            supply,
+                                        )
+                                        for raw_element in raw_bill["elements"]:
+                                            bill.insert_element(
+                                                sess,
+                                                raw_element["name"],
+                                                raw_element["start_date"],
+                                                raw_element["finish_date"],
+                                                raw_element["net"],
+                                                raw_element["breakdown"],
+                                            )
+                                        for raw_read in raw_bill["reads"]:
+                                            bill.insert_read(
+                                                sess,
+                                                tprs[raw_read["tpr_code"]],
+                                                raw_read["coefficient"],
+                                                raw_read["units"],
+                                                raw_read["msn"],
+                                                raw_read["mpan"],
+                                                raw_read["prev_date"],
+                                                raw_read["prev_value"],
+                                                read_types[raw_read["prev_type_code"]],
+                                                raw_read["pres_date"],
+                                                raw_read["pres_value"],
+                                                read_types[raw_read["pres_type_code"]],
+                                            )
+                                        self.successful_bills.append(raw_bill)
+                                except KeyError as e:
+                                    err = raw_bill.get("error", "")
+                                    raw_bill["error"] = err + " " + str(e)
+                                    self.failed_bills.append(raw_bill)
+                                except BadRequest as e:
+                                    raw_bill["error"] = str(e.description)
+                                    self.failed_bills.append(raw_bill)
+
+                    if len(self.failed_bills) == 0:
+                        sess.commit()
+                        self._log(
+                            "All the bills have been successfully loaded and attached "
+                            "to the batch."
+                        )
+                    else:
+                        sess.rollback()
+                        self._log(
+                            f"The import has finished, but there were "
+                            f"{len(self.failed_bills)} "
+                            f"failures, and so the whole import has been rolled back."
+                        )
+                        break
 
         except BadRequest as e:
             msg = f"Problem: {e.description}"
