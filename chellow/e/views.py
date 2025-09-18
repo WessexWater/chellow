4c1567927110ecea601cd05a3cf4fb8f8e314080
Tony Locke <tlocke@tlocke.org.uk>
2025-09-18 14:37:05 +0100

    i

--- chellow/e/views.py
+++ chellow/e/views.py
@@ -779,9 +779,9 @@ def dc_batch_file_download_get(file_id):
     batch_file = BatchFile.get_by_id(g.sess, file_id)
 
     output = make_response(batch_file.data)
-    output.headers[
-        "Content-Disposition"
-    ] = f'attachment; filename="{batch_file.filename}"'
+    output.headers["Content-Disposition"] = (
+        f'attachment; filename="{batch_file.filename}"'
+    )
     output.headers["Content-type"] = "application/octet-stream"
     return output
 
@@ -2948,9 +2948,9 @@ def mop_batch_file_download_get(file_id):
     batch_file = BatchFile.get_by_id(g.sess, file_id)
 
     output = make_response(batch_file.data)
-    output.headers[
-        "Content-Disposition"
-    ] = f'attachment; filename="{batch_file.filename}"'
+    output.headers["Content-Disposition"] = (
+        f'attachment; filename="{batch_file.filename}"'
+    )
     output.headers["Content-type"] = "application/octet-stream"
     return output
 
@@ -5353,9 +5353,9 @@ def supplier_batch_file_download_get(file_id):
     batch_file = BatchFile.get_by_id(g.sess, file_id)
 
     output = make_response(batch_file.data)
-    output.headers[
-        "Content-Disposition"
-    ] = f'attachment; filename="{batch_file.filename}"'
+    output.headers["Content-Disposition"] = (
+        f'attachment; filename="{batch_file.filename}"'
+    )
     output.headers["Content-type"] = "application/octet-stream"
     return output
 
