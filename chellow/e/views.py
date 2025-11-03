ab7f8400a764022b03abca0a76ea0d2047cbc961
Tony Locke <tlocke@tlocke.org.uk>
2025-11-03 11:33:15 +0000

    Batch Delete: Redirect is broken

--- chellow/e/views.py
+++ chellow/e/views.py
@@ -780,9 +780,9 @@ def dc_batch_file_download_get(file_id):
     batch_file = BatchFile.get_by_id(g.sess, file_id)
 
     output = make_response(batch_file.data)
-    output.headers["Content-Disposition"] = (
-        f'attachment; filename="{batch_file.filename}"'
-    )
+    output.headers[
+        "Content-Disposition"
+    ] = f'attachment; filename="{batch_file.filename}"'
     output.headers["Content-type"] = "application/octet-stream"
     return output
 
@@ -2950,9 +2950,9 @@ def mop_batch_file_download_get(file_id):
     batch_file = BatchFile.get_by_id(g.sess, file_id)
 
     output = make_response(batch_file.data)
-    output.headers["Content-Disposition"] = (
-        f'attachment; filename="{batch_file.filename}"'
-    )
+    output.headers[
+        "Content-Disposition"
+    ] = f'attachment; filename="{batch_file.filename}"'
     output.headers["Content-type"] = "application/octet-stream"
     return output
 
@@ -5243,7 +5243,7 @@ def supplier_batch_edit_delete(batch_id):
         contract_id = batch.contract.id
         batch.delete(g.sess)
         g.sess.commit()
-        return hx_redirect(f"/supplier_batches?supplier_contract_id={contract_id}", 303)
+        return hx_redirect(f"/supplier_contracts/{contract_id}/batches", 303)
     except BadRequest as e:
         flash(e.description)
         return make_response(
@@ -5364,9 +5364,9 @@ def supplier_batch_file_download_get(file_id):
     batch_file = BatchFile.get_by_id(g.sess, file_id)
 
     output = make_response(batch_file.data)
-    output.headers["Content-Disposition"] = (
-        f'attachment; filename="{batch_file.filename}"'
-    )
+    output.headers[
+        "Content-Disposition"
+    ] = f'attachment; filename="{batch_file.filename}"'
     output.headers["Content-type"] = "application/octet-stream"
     return output
 
