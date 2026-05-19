29db719d11ea88555472aa4a1092bced7eb35ed4
Tony Locke <tlocke@tlocke.org.uk>
2026-05-19 15:39:53 +0100

    Channel snags not shown

--- test/reports/test_report_channel_snags.py
+++ test/reports/test_report_channel_snags.py
@@ -21,6 +21,7 @@ from chellow.models import (
     Participant,
     Pc,
     Site,
+    Snag,
     Source,
     User,
     UserRole,
@@ -211,6 +212,141 @@ def test_do_get_as_html(mocker, sess, client):
     }
     response = client.get("/reports/channel_snags", query_string=query_string)
     match(response, 200)
+
+
+def test_do_get_as_html_with_issue(mocker, sess, client):
+    vf = to_utc(ct_datetime(2000, 1, 1))
+    site = Site.insert(sess, "CI017", "Water Works")
+    market_role_Z = MarketRole.get_by_code(sess, "Z")
+    participant = Participant.insert(sess, "CALB", "AK Industries")
+    non_core_party = participant.insert_party(
+        sess, market_role_Z, "None core", vf, None, None
+    )
+    market_role_C = MarketRole.insert(sess, "C", "HH Dc")
+    dc_party = participant.insert_party(
+        sess, market_role_C, "Fusion DC", vf, None, None
+    )
+    dc_contract = dc_party.insert_contract(sess, "Fusion DC 2000", "", {}, vf, None, {})
+    dc_contract_id = dc_contract.id
+    bank_holiday_rate_script = {"bank_holidays": []}
+    non_core_party.insert_contract(
+        sess, "bank_holidays", "", {}, vf, None, bank_holiday_rate_script
+    )
+    market_role_X = MarketRole.insert(sess, "X", "Supplier")
+    market_role_M = MarketRole.insert(sess, "M", "Mop")
+    market_role_R = MarketRole.insert(sess, "R", "Distributor")
+    mop_party = participant.insert_party(
+        sess, market_role_M, "Fusion Mop Ltd", vf, None, None
+    )
+    supplier_party = participant.insert_party(
+        sess, market_role_X, "Fusion Ltc", vf, None, None
+    )
+
+    mop_contract = mop_party.insert_contract(
+        sess, "Fusion Mop Contract", "", {}, vf, None, {}
+    )
+    pc = Pc.insert(sess, "00", "hh", vf, None)
+    insert_cops(sess)
+    cop = Cop.get_by_code(sess, "5")
+    insert_comms(sess)
+    comm = Comm.get_by_code(sess, "GSM")
+    imp_supplier_contract = supplier_party.insert_contract(
+        sess, "Fusion Supplier 2000", "", {}, vf, None, {}
+    )
+    dno_party = participant.insert_party(sess, market_role_R, "WPD", vf, None, "22")
+    dno_party.insert_contract(sess, dno_party.dno_code, "", {}, vf, None, {})
+    meter_type = MeterType.insert(sess, "C5", "COP 1-5", vf, None)
+    meter_payment_type = MeterPaymentType.insert(sess, "CR", "Credit", vf, None)
+    mtc = Mtc.insert(sess, "845", False, True, vf, None)
+    mtc_participant = MtcParticipant.insert(
+        sess,
+        mtc,
+        participant,
+        "HH COP5 And Above With Comms",
+        False,
+        True,
+        meter_type,
+        meter_payment_type,
+        0,
+        vf,
+        None,
+    )
+    insert_voltage_levels(sess)
+    voltage_level = VoltageLevel.get_by_code(sess, "HV")
+    llfc = dno_party.insert_llfc(
+        sess, "510", "PC 5-8 & HH HV", voltage_level, False, True, vf, None
+    )
+    MtcLlfc.insert(sess, mtc_participant, llfc, vf, None)
+    insert_sources(sess)
+    source = Source.get_by_code(sess, "grid")
+    gsp_group = GspGroup.insert(sess, "_L", "South Western")
+    insert_energisation_statuses(sess)
+    energisation_status = EnergisationStatus.get_by_code(sess, "E")
+    insert_dtc_meter_types(sess)
+    dtc_meter_type = DtcMeterType.get_by_code(sess, "H")
+    supply = site.insert_e_supply(
+        sess,
+        source,
+        None,
+        "Bob",
+        to_utc(ct_datetime(2000, 1, 1)),
+        None,
+        gsp_group,
+        mop_contract,
+        dc_contract,
+        "hgjeyhuw",
+        dno_party,
+        pc,
+        "845",
+        cop,
+        comm,
+        None,
+        energisation_status,
+        dtc_meter_type,
+        "22 7867 6232 781",
+        "510",
+        imp_supplier_contract,
+        "7748",
+        361,
+        None,
+        None,
+        None,
+        None,
+        None,
+    )
+    era = supply.eras[0]
+    era.insert_channel(sess, True, "ACTIVE")
+    issue = imp_supplier_contract.insert_issue(
+        sess, to_utc(ct_datetime(1997, 1, 1)), {}
+    )
+    issue.attach_supply(supply)
+    sess.commit()
+
+    days_hidden = 0
+    is_ignored = True
+    show_settlement = "both"
+    only_ongoing = True
+    days_long_hidden = ""
+    query_string = {
+        "days_hidden": days_hidden,
+        "show_settlement": show_settlement,
+        "is_ignored": is_ignored,
+        "only_ongoing": only_ongoing,
+        "dc_contract_id": dc_contract_id,
+        "days_long_hidden": days_long_hidden,
+        "comm_id": comm.id,
+    }
+    response = client.get("/reports/channel_snags", query_string=query_string)
+    for snag in sess.scalars(select(Snag)):
+        print(
+            snag.id,
+            snag,
+            "channel",
+            snag.channel.id,
+            "contract id",
+            snag.channel.era.dc_contract.id,
+        )
+    match(response, 200, r"<td>Missing</td>")
 
 
 def test_content(mocker, sess):
